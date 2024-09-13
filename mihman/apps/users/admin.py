9d45fb65e6217a29c992abc8fe81e23ee56bcd28
mdmubi789 <mdmubi789@gmail.com>
2024-09-13 10:08:41 +0530

    user app committed

--- mihman/apps/users/admin.py
+++ mihman/apps/users/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    # List the fields you want to display in the user list view
+    list_display = ['username', 'mb_number_or_email'] 
+
+    # Define the fields you want to use for the user creation form
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'mb_number_or_email', 'password1', 'password2'),
+        }),
+    )
+
+    # Define the fields you want to use for the user change form
+    fieldsets = (
+        (None, {'fields': ('username', 'mb_number_or_email', 'password')}),
+    )
+    
+    # Required for handling user creation
+    filter_horizontal = ()
+    ordering = ('username',)
+
+admin.site.register(CustomUser, CustomUserAdmin)
--- mihman/apps/users/models.py
+++ mihman/apps/users/models.py
@@ -1,3 +1,27 @@
+from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    mb_number_or_email = models.CharField(max_length=254, unique=True, blank=False)
+
+    groups = models.ManyToManyField(
+        Group,
+        related_name='customuser_set',  # Add unique related_name
+        blank=True,
+    )
+
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name='customuser_permissions_set',  # Add unique related_name
+        blank=True,
+    )
+
+    def __str__(self):
+        return self.email or self.username
+    
+    def serialize(self):
+        return {
+            'id': self.id,
+            'username': self.username,
+            'mb_number_or_email': self.mb_number_or_email
+        }
